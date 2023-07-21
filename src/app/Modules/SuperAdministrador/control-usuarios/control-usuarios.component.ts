65e8f448f592fe2e747cc5854b44a147709bc56e
EstebanBacuilima <davidbacuilima13@gmail.com>
2023-07-21 15:42:32 -0500

    New Cahnges

--- src/app/Modules/SuperAdministrador/control-usuarios/control-usuarios.component.ts
+++ src/app/Modules/SuperAdministrador/control-usuarios/control-usuarios.component.ts
@@ -65,24 +65,16 @@ export class ControlUsuariosComponent implements OnInit {
 
 
 
-  // listRoles
-  isRoleAssigned(role: Rol): boolean {
-    return this.listRoleAsignarUser.some(
-      (assignedRole) => assignedRole.idRol === role.idRol
-    );
-  }
-
   // ADD UPDATE
   public editUsuario(usuario: Usuario) {
     this.usuario = { ...usuario };
     this.persona = this.usuario.persona;
-    this.listRoleAsignarUser = [...this.usuario.roles];
     this.userDialog = true;
   }
 
   // GET ALL ROLES
   public listRoles: Rol[] = [];
-  
+
   public getAllRolesFull() {
     this.rolesService.getAllRolesFull().subscribe((data) => {
       this.listRoles = data;
@@ -93,22 +85,7 @@ export class ControlUsuariosComponent implements OnInit {
 
 
   //ASIGNAR ROLES A USUARIO
-  public listRoleAsignarUser: Rol[] = [];
-
-  public asignarRolesUsuario(rol: Rol) {
-    const index = this.listRoleAsignarUser.findIndex(
-      (item) => item.idRol === rol.idRol
-    );
-    if (index !== -1) {
-      // Si el rol ya existe, lo eliminamos del arreglo
-      this.listRoleAsignarUser.splice(index, 1);
-    } else {
-      // Si el rol no existe, lo agregamos al arreglo
-      this.listRoleAsignarUser.push(rol);
-    }
-    console.log(this.listRoleAsignarUser);
-  }
-
+  public selectedRoles: Rol[] = [];
 
   // ADD NEW USUARIO
   public submitted: boolean = false;
@@ -117,18 +94,31 @@ export class ControlUsuariosComponent implements OnInit {
   usuario = new Usuario();
 
   public async saveNewUsuario() {
+
+    let nombre1;
+    let apellido1;
+
+    if (this.fullname.includes(" ")) {
+      let partesNombre = this.fullname.split(" ");
+      nombre1 = partesNombre[0];
+      apellido1 = partesNombre.slice(1).join(" ");
+    }
+
+    this.persona.nombre1 = nombre1;
+    this.persona.apellido1 = apellido1;
+    
     const key = await this.uploadImage();
     this.personaService.savePersona(this.persona).subscribe((data) => {
       this.persona = data
       console.log(this.persona)
       this.usuario.persona = this.persona
-      this.usuario.roles = this.listRoleAsignarUser
+      this.usuario.roles = this.selectedRoles
       this.usuario.estadoUsuario = true;
       this.usuario.fotoPerfil = key;
       this.usuario.tokenPassword = '';
       this.usuarioService.saveUsuario(this.usuario).subscribe((data) => {
-          this.usuario = data;
-          alert('SUCESSFULL')
+        this.usuario = data;
+        alert('SUCESSFULL')
       }, (error) => {
         console.log('2', error)
       })
@@ -140,7 +130,7 @@ export class ControlUsuariosComponent implements OnInit {
 
   // UPDATE USUARIO
   public updateUsuario() {
-    this.usuario.roles = this.listRoleAsignarUser
+    this.usuario.roles = this.selectedRoles
     this.usuarioService.saveUsuario(this.usuario).subscribe((data) => {
       this.usuario = data;
     }, (error) => {
@@ -169,7 +159,6 @@ export class ControlUsuariosComponent implements OnInit {
     this.usuario = {} as Usuario;
     this.userDialog = true;
     this.fullname = '';
-    this.listRoleAsignarUser = [];
   }
 
   // TABLE
@@ -222,7 +211,7 @@ export class ControlUsuariosComponent implements OnInit {
     this.submitted = false;
     this.persona = {} as Persona;
     this.usuario = {} as Usuario;
-    this.userDialog = true;
+    this.userDialog = false;
     this.fullname = '';
   }
 
